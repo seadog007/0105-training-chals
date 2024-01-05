a184a895f46989107b738005ca68a164191d23df
Ubuntu <seadog007@seadog007-daily.103.147.22.142>
2024-01-05 05:08:52 +0000

    Fixed the code

--- revchal-1/chal.c
+++ revchal-1/chal.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     // Write C code here
     char a[20];
     scanf("%s", a);
+    printf("Please enter the password:\n");
     if (!strcmp(a, "THi5_15_the_P4ssw0rd")){
         printf("Correct Password!\n");
-        printf("FLAG{0105_rev_chal}");
+        printf("FLAG{0105_rev_chal}\n");
     } else {
-        printf("Wrong Password");
+        printf("Wrong Password\n");
     }
 
     return 0;
